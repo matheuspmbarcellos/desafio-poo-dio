ccd17b51fd94b92cd74ccf5d60593ca0d4dce058
Matheus Barcellos <matbarcellos21@gmail.com>
2023-02-04 00:02:39 -0300

    create class domains

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 }
